1ee885f716ff987f3efa70d6e943321eabb78c28
cd-34 <85720817+cd-34@users.noreply.github.com>
2026-03-17 03:27:28 -0400

    improve upon moveScanner method to only accept legal moves, but may need to be updated to throw errors instead of print statements

--- tictactoe/tictactoe.java
+++ tictactoe/tictactoe.java
@@ -12,6 +12,7 @@ public class TicTacToe {
     }    
 
     public static void initGame() {
+        // this method will initialize char[][] board to ABCEDFGHI
         String boardLetters = "ABCDEFGHI";
         for (int i = 0; i < board.length; i++) {
             for (int j = 0; j < board.length; j++) {
@@ -29,19 +30,42 @@ public class TicTacToe {
         System.out.println("Player " + playerTurn + "'s turn! Enter your move.");
     }
 
-
+    // first time using static void main() method, not sure if this is the proper implementation
+    // because this is static, I feel like I'm forced make my other methods static as well
+    // otherwise the compiler will complain because a non-static method is being called within this static main() method
+    // what's the solution?
     public static void main() {
         initGame();
         printBoard();
         moveScanner();
     }
 
-    public static void moveScanner() {
-        Scanner move = new Scanner(System.in);
-        String input = move.nextLine();
-        System.out.println(input);
-    }
+    public static char moveScanner() {
+        Scanner sc = new Scanner(System.in);
+        String moveString = sc.nextLine();
+        char moveChar = moveString.charAt(0);
+    
+        if (moveString.length() == 0) {
+            // produces compiler errors but continues to run fine
+            // if I tried to throw new error, it would exit the if statement and not recurse
+            // TODO find if there's a way to throw an exception but continue the if statement
+            System.out.println("Please enter a character."); 
+            moveScanner();
+        } else if (moveString.length() > 1) {
+            System.out.println("Please enter one character.");
+            moveScanner();
+        } else if (moveString.toUpperCase().equals("X") || moveString.toUpperCase().equals("O")) {
+            System.out.println("This space is already occupied!");
+            moveScanner();
+        } else if (Character.getNumericValue(moveChar) < 10 || Character.getNumericValue(moveChar) > 18) {
+            System.out.println("Enter a legal space from A to I");
+            moveScanner();
+        }
 
+        char moveCharTemp = moveString.charAt(0);
+        // returns the character input needed to manipulate the board later
+        return Character.toUpperCase(moveCharTemp);
+    }
 }
 
 // TicTacToe newGame = new TicTacToe();
