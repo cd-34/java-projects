f19926b3aa20187dd40e9ee8ab06235bd6c9a0df
cd-34 <85720817+cd-34@users.noreply.github.com>
2026-03-24 15:47:02 -0400

    separate public static void main into its own class Main

---
+++ tictactoe-project/tictactoe/Main.java
@@ -0,0 +1,21 @@
+package tictactoe;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        System.out.println("Please enter player 1's name.");
+        Player player1 = new Player(InputHandler.nameScanner(scan));
+        System.out.println("Welcome " + player1.getName() + "!");
+
+        System.out.println("Please enter player 2's name.");
+        Player player2 = new Player(InputHandler.nameScanner(scan));
+        System.out.println("Welcome " + player2.getName() + "!");
+        // System.out.println(player1.getName() + " " + player2.getName());
+        TicTacToe game = new TicTacToe(player1, player2, scan);
+        game.run();
+
+        scan.close();
+    }
+}
--- tictactoe-project/tictactoe/TicTacToe.java
+++ tictactoe-project/tictactoe/TicTacToe.java
@@ -70,23 +70,6 @@ public class TicTacToe {
         System.out.println(playerName + "'s turn (" + symbol + ")! Enter your move (e.g. A1, B2, C3):");
     }
 
-    public static void main(String[] args) {
-        Scanner scan = new Scanner(System.in);
-
-        System.out.println("Please enter player 1's name.");
-        Player player1 = new Player(InputHandler.nameScanner(scan));
-        System.out.println("Welcome " + player1.getName() + "!");
-
-        System.out.println("Please enter player 2's name.");
-        Player player2 = new Player(InputHandler.nameScanner(scan));
-        System.out.println("Welcome " + player2.getName() + "!");
-        // System.out.println(player1.getName() + " " + player2.getName());
-        TicTacToe game = new TicTacToe(player1, player2, scan);
-        game.run();
-
-        scan.close();
-    }
-
     public void run() {
         boolean playing = true;
         while (playing) {
