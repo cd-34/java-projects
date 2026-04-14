6acf0c0aafb21e2bb70ba8fda6a7498cfabfeda0
cd-34 <85720817+cd-34@users.noreply.github.com>
2026-04-14 15:10:07 -0400

    change boardSize to BOARD_SIZE

--- tictactoe-project/tictactoe/Main.java
+++ tictactoe-project/tictactoe/Main.java
@@ -2,12 +2,12 @@ package tictactoe;
 import java.util.Scanner;
 
 public class Main {
-    private static final int boardSize = 3;
+    private static final int BOARD_SIZE = 3;
     
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
 
-        InputHandler inputHandler = new InputHandler(scan, boardSize);
+        InputHandler inputHandler = new InputHandler(scan, BOARD_SIZE);
 
         System.out.println("Please enter player 1's name.");
         Player player1 = new Player(inputHandler.nameScanner(scan), 'X');
@@ -17,7 +17,7 @@ public class Main {
         Player player2 = new Player(inputHandler.nameScanner(scan), 'O');
         System.out.println("Welcome " + player2.getName() + "!");
 
-        Game game = new Game(player1, player2, inputHandler, scan, boardSize);
+        Game game = new Game(player1, player2, inputHandler, scan, BOARD_SIZE);
         game.run();
 
         scan.close();
