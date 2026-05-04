6a92bf5b055b7ecd64535f4f6276303aa6083de7
cd-34 <85720817+cd-34@users.noreply.github.com>
2026-05-04 18:53:00 -0400

    add askPlayerName and askPlayerCount

--- 03-blackjack-project/blackjack/ConsoleIO.java
+++ 03-blackjack-project/blackjack/ConsoleIO.java
@@ -1,2 +1,39 @@
 package blackjack;
 import java.util.Scanner;
+
+public class ConsoleIO {
+    private Scanner scan;
+
+    public ConsoleIO(Scanner scan) {
+        this.scan = scan;
+    }
+
+    public static String askPlayerName(Scanner scan) {
+        while (true) {
+            String inputName = scan.nextLine();
+
+            if (inputName.isBlank()) {
+                System.out.println("Invalid input - please enter your name.");
+                continue;
+            }
+            return inputName;
+        }
+    }
+
+    public static int askPlayerCount(Scanner scan) {
+        while (true) {
+            if (scan.hasNextInt()) {
+                int playerCount = scan.nextInt();
+                scan.nextLine();
+                if (playerCount >= 1 && playerCount <= 4) {
+                    return playerCount;
+                } else {
+                    System.out.println("This game only supports 1-4 players. Please enter a number again.");
+                }
+            } else {
+                System.out.println("Please enter a number between 1-4.");
+                scan.nextLine();
+            }
+        }
+    }
+}
--- 03-blackjack-project/blackjack/Main.java
+++ 03-blackjack-project/blackjack/Main.java
@@ -4,5 +4,12 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello World!");
+        Scanner scan = new Scanner(System.in);
+        Player player1 = new Player(ConsoleIO.askPlayerName(scan));
+        System.out.println("Welcome " + player1.getName() + "!");
+
+        int playerCount = ConsoleIO.askPlayerCount(scan);
+        System.out.println(playerCount);
+        scan.close();
     }
 }
