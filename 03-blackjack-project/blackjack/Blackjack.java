05d674c1a86e2ea4bdfe6897d2d78c52c9fe7548
cd-34 <85720817+cd-34@users.noreply.github.com>
2026-05-11 06:04:21 -0400

    move some testing into init()

--- 03-blackjack-project/blackjack/Blackjack.java
+++ 03-blackjack-project/blackjack/Blackjack.java
@@ -3,10 +3,13 @@ package blackjack;
 public class Blackjack {
     public void init() {
         // next 5 lines are just temporary so I can see the deck being shuffled and printed out
+        // Deck deck = new Deck();
+        // deck.shuffle();
+        // for (Card card : deck.getCards()) {
+        //     System.out.println(card + "\n");
+        // }
         Deck deck = new Deck();
         deck.shuffle();
-        for (Card card : deck.getCards()) {
-            System.out.println(card + "\n");
-        }
+        System.out.println(deck.size()); // should be 52
     }
 }
--- 03-blackjack-project/blackjack/BlackjackApp.java
+++ 03-blackjack-project/blackjack/BlackjackApp.java
@@ -27,14 +27,14 @@ public class BlackjackApp {
 
         // temporarily add two cards to player1's hand then print them out to test
         // remove later
-        Deck deck = new Deck();
-        deck.shuffle();
-        System.out.println(deck.size()); // should be 52
-        players[0].addCard(deck.deal());
-        players[0].addCard(deck.deal());
-        System.out.println(players[0].getName() + "'s hand:");
-        System.out.println(players[0].getHand());
-        System.out.println(deck.size()); // should be 50
+        // Deck deck = new Deck();
+        // deck.shuffle();
+        // System.out.println(deck.size()); // should be 52
+        // players[0].addCard(deck.deal());
+        // players[0].addCard(deck.deal());
+        // System.out.println(players[0].getName() + "'s hand:");
+        // System.out.println(players[0].getHand());
+        // System.out.println(deck.size()); // should be 50
     }
 
     // initializes a game and starts playGame() until there is a winner
